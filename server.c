348d2ddb556247190d92a40c11a91918d46e8dc1
Matheus Soares <matheus_03@hotmail.com>
2018-09-29 02:08:49 -0300

    Add files via upload
    Add first code server

---
+++ server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <dirent.h>
+
+int main(){
+
+	struct sockaddr_in socketAddr; 
+
+	socketAddr.sin_family = AF_INET;						// Familia do endereço
+	socketAddr.sin_port = htons(7000);						// Porta escolhida (htons converte o valor na ordem de bytes da rede)
+	socketAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	// Endereço IP (inet_addr converte o endereço para binario em bytes)
+	bzero(&(socketAddr.sin_zero), 8);						// Estrutura com zeros sem utilidade (Para propósitos futuros)?
+
+	int socketR = socket(AF_INET, SOCK_STREAM, 0);  	// Initializa o socket. 	(Domínio da comunicação (AF_NET p/ TCP/IP),
+														// 							Tipo de comunicação (TCP/UDP) (SOCK_STREAM p/ TCP)
+														//							Protocolo (0))
+		
+	if(socketR == -1){	// A função socket() retorna -1 se ocorrer algum erro
+		printf ("Error creating the socket");
+		return -1;
+	}
+
+	int bindR = bind(socketR, (struct sockaddr *)&socketAddr, sizeof(socketAddr));	// Associa o socket criado com a porta do SO	
+	
+	if(bindR == -1){	// A função bind() retorna -1 se ocorrer algum erro e 0 caso contrário
+		printf("Error in function bind\n");
+		return -1;
+	}
+
+	int listenR = listen(socketR, 10); 		// Habilita o socket pra receber as conexões através do descritor (socketR) do socket
+
+	if(listenR == -1){
+		printf("Error in function listen\n");
+		return -1;
+	}
+
+	while(1){
+		
+		int acceptR= accept(socketR, (struct sockaddr*)NULL, NULL); 	// Aceita conexões
+		
+		if(acceptR == -1){
+			printf("Error in function accept\n");
+			return -1;
+		}
+
+		printf("Socket criado: %d\n", acceptR);
+	}
+
+
+	return 0;
+}
